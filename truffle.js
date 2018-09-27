3affa5c466dfd21dd521e8a699a652178f584d7a
Michal <michal83h@gmail.com>
2018-09-27 13:01:29 +0200

    config

--- truffle.js
+++ truffle.js
@@ -4,8 +4,6 @@ module.exports = {
       host: "ganache",
       port: 8545,
       network_id: "*",
-      gasPrice: 20000000000,
-      gas: 90000,
       from: '0xb3e64e1b9173239adb5c1daf3067bb4c958e2f8b'
     }
   }
