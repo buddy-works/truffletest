e855d2c2a4d68b2db964dc4acc2ad06b9e231463
Rafał Sztwiorok <rafal.sztwiorok@gmail.com>
2018-09-27 15:13:28 +0200

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port: 8545,
       network_id: "*",
       from: '0xb3e64e1b9173239adb5c1daf3067bb4c958e2f8b'
+    },
+    production: {
+      host: "ganache",
+      port: 8545,
+      network_id: "*",
+      from: '0xb3e64e1b9173239adb5c1daf3067bb4c958e2f8b'
     }
   }
 };
