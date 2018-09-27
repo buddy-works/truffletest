23c2f0352bf2d6f5b56056723339776feb3784e5
Michal <michal83h@gmail.com>
2018-09-27 13:00:22 +0200

    config

--- truffle.js
+++ truffle.js
@@ -6,7 +6,7 @@ module.exports = {
       network_id: "*",
       gasPrice: 20000000000,
       gas: 90000,
-      from: '0xc3df2af882d9dc6ff537963c24beb3bb9dfa712b'
+      from: '0xb3e64e1b9173239adb5c1daf3067bb4c958e2f8b'
     }
   }
 };
